6c33b6462fcb0e9661bd9f570a6edcd76007bc63
ibz1001 <52083742+ibz1001@users.noreply.github.com>
2022-03-30 11:26:38 +0100

    lorenz

---
+++ sketches/lorenz.js
@@ -0,0 +1,56 @@
+let scl = 11;
+
+let sigma = 10;
+let rho = 28;
+let beta = 8.0 / 3.0;
+
+let dt = 0.005;
+
+let x = [];
+let y = [];
+let z = [];
+
+let px = [];
+let py = [];
+let pz = [];
+
+let amount = 500;
+
+
+function setup() {
+  createCanvas(800, 800);
+  
+  for (let i = 0; i < amount + 1; i++){
+    x.push(random(-0.01,0.01));
+    y.push(random(-0.01,0.01));
+    z.push(random(-0.01,0.01));
+    
+    px.push(x[x.length - 1]);
+    py.push(y[y.length - 1]);
+    pz.push(z[z.length - 1]);
+  }
+  stroke(255, 100)
+}
+
+function draw() {
+  clear()
+
+  
+  translate(width / 2, height / 2)
+  for (let i = 0; i < amount;i++){
+    var dx = (sigma * (y[i] - x[i])) * dt;
+    var dy = (x[i] * (rho - z[i]) - y[i]) * dt;
+    var dz = (x[i] * y[i] - beta * z[i]) * dt; 
+    
+    px[i] = x[i];
+    py[i] = y[i];
+    pz[i] = z[i];
+    
+    x[i] += dx;
+    y[i] += dy;
+    z[i] += dz;
+    
+    line(px[i] * scl, py[i] * scl, x[i] * scl, y[i] * scl)
+    
+  }
+}
